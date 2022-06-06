9f377481aa1358ee2504f9c9e2ff718a4db67419
Ibnul Ashir <ibnulashir42@gmail.com>
2022-06-06 23:37:18 +0600

    footer completed

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin, aistake } from "react-icons/ai";
+import { Link } from "react-router-dom";
+// import {AiFillLinkedin}
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="max-w-7xl mx-auto my-3 flex justify-center gap-4">
+      <div className="">
+        <a href="https://twitter.com" target="_blank">
+          <AiFillGithub fill="#fff" className="w-8 h-8" />
+        </a>
+      </div>
+      <div className="">
+        <a href="https://linkedin.com" target="_blank">
+          <AiFillLinkedin fill="#fff" className="w-8 h-8" />
+        </a>
+      </div>
+      <div className="">
+        <a href="https://stackoverflow.com/" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            className="w-8 h-8"
+          >
+            <path
+              d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z"
+              fill="#fff"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div className="max-w-7xl mx-auto h-screen flex flex-col justify-between">
+    <div className="max-w-7xl mx-auto h-screen flex flex-col justify-between bg-[#223a5b]">
       <Header />
       <Content />
       <Footer />
